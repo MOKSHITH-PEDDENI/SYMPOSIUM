6c79d92c15978b6c2ab2073f4090fb835c921332
MOKSHITH-PEDDENI <peddenimokshith@gmail.com>
2025-02-21 16:01:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Toggle mobile menu
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
